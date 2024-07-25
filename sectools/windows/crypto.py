fda6a88d5f6fef4a90bb8d53a6d06d476a297051
Rémi GASCOU (Podalirius) <podalirius@protonmail.com>
2024-07-25 08:10:31 +0200

    Update crypto.py, fixes #9

--- sectools/windows/crypto.py
+++ sectools/windows/crypto.py
@@ -12,15 +12,16 @@ def parse_lm_nt_hashes(lm_nt_hashes_string):
     lm_hash_value, nt_hash_value = "", ""
     if lm_nt_hashes_string is not None:
         matched = re.match("([0-9a-f]{32})?(:)?([0-9a-f]{32})?", lm_nt_hashes_string.strip().lower())
-        m_lm_hash, m_sep, m_nt_hash = matched.groups()
-        if m_lm_hash is None and m_sep is None and m_nt_hash is None:
-            lm_hash_value, nt_hash_value = "", ""
-        elif m_lm_hash is None and m_nt_hash is not None:
+        m_lm_hash, _, m_nt_hash = matched.groups()
+        if m_lm_hash is None and m_nt_hash is not None:
             lm_hash_value = "aad3b435b51404eeaad3b435b51404ee"
             nt_hash_value = m_nt_hash
         elif m_lm_hash is not None and m_nt_hash is None:
             lm_hash_value = m_lm_hash
             nt_hash_value = nt_hash("")
+        else:
+            lm_hash_value = m_lm_hash
+            nt_hash_value = m_nt_hash
     return lm_hash_value, nt_hash_value
 
 
